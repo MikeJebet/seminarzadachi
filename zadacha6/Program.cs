536bdfe16f1aa54464e8bf3877197afb37da2091
Mikhail <mishaforever96@mail.ru>
2022-08-23 17:39:44 +0300

    zadacha6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+string? numberAu = Console.ReadLine();
+int numberAe;
+if(int.TryParse(numberAu, out numberAe))
+{
+    if(numberAe % 2 == 0)
+    {
+        Console.WriteLine("Число четное");
+    }
+    else
+    {
+        Console.WriteLine("Число нечетное");
+    }
+}
+else 
+{
+Console.WriteLine("Error");    
+}

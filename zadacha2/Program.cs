053b4927b0ca586db682742772c01d637cedcf73
Mikhail <mishaforever96@mail.ru>
2022-08-23 16:20:40 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число: ");
+string? numberAu = Console.ReadLine();
+int numberAe;
+Console.WriteLine("Введите число: ");
+string? numberBu = Console.ReadLine();
+int numberBe;
+if (int.TryParse(numberAu, out numberAe))
+{
+    if (int.TryParse(numberBu, out numberBe))
+    {
+        if (numberAe > numberBe)
+        {
+            Console.WriteLine("numberAe the biggest one");
+        }
+        else
+        {
+            Console.WriteLine("numberBe the biggest one");
+        }
+        if (numberAe == numberBe)
+        {
+            Console.WriteLine("numberAe = numberBe");
+        }
+
+    }
+}
+else 
+{
+    Console.WriteLine("Error");
+}

1ab34029672b443162242929b8b5ff32ab4266ff
Mikhail <mishaforever96@mail.ru>
2022-08-23 18:06:28 +0300

    zadacha8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+string? numberAu = Console.ReadLine();
+int numberAe;
+int i = 1;
+int N = 0;
+if(int.TryParse(numberAu, out numberAe))
+{
+Console.WriteLine("Четные числа: ");
+    N = numberAe;
+    while(i < N)
+    {
+        if(i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+        i += 1;
+
+    }
+}
+else 
+{
+Console.WriteLine("Error");    
+}

953587de2c5c0d3cb722d64b58e4ee727b8d794e
Mikhail <mishaforever96@mail.ru>
2022-08-25 02:19:34 +0300

    zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string? numberA = Console.ReadLine();
+if(numberA.Length > 2)
+    {
+    Console.WriteLine(numberA[2]);
+    }
+else 
+    {
+    Console.WriteLine("Третьей цифры нет");
+    }

8488966a87a946bf773356160ec0b7447191b0ab
Mikhail <mishaforever96@mail.ru>
2022-08-25 03:11:00 +0300

    zadacha 15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите цифру дня недели от 1 до 7: ");
+string? numberAu = Console.ReadLine();
+int numberAe;
+if(int.TryParse(numberAu, out numberAe))
+{
+    if(numberAe == 6)
+    {
+        Console.WriteLine("Сегодня выходной!!!");
+    }
+    else if(numberAe == 7)
+    {
+        Console.WriteLine("Сегодня выходной!!!");
+    }
+    else if(numberAe > 7)
+    {
+        Console.WriteLine("Error");
+    }
+    else if(numberAe < 1)
+    {
+        Console.WriteLine("Error");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
+else 
+    {
+    Console.WriteLine("Error");
+    }

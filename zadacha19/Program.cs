d024f5344956855637f9ab687df4ef563b6c8841
Mikhail <mishaforever96@mail.ru>
2022-08-26 18:38:04 +0300

    zadacha 19

---
+++ zadacha19/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string? numberAu = Console.ReadLine();
+int numberAe = 0;
+if (int.TryParse(numberAu, out numberAe))
+{
+int oldnum = numberAe;
+int newnum = 0;
+    while (numberAe > 0)
+    {
+        int dig = numberAe % 10;
+        newnum = newnum * 10 + dig;
+        numberAe = numberAe / 10;
+    }
+    if (newnum == oldnum)
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
+else 
+{
+    Console.WriteLine("Error");
+}

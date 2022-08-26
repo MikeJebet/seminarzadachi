2b7159e1c7efb370a9666fea90b4df8b03dcf84f
Mikhail <mishaforever96@mail.ru>
2022-08-26 19:52:14 +0300

    zadacha 23

---
+++ zadacha23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число : ");
+string? numberAu = Console.ReadLine();
+Console.WriteLine();
+int numberAe;
+int N = 0;
+int i = 1;
+if(int.TryParse(numberAu, out numberAe))
+{
+    N = numberAe;
+    while(i < N)
+    {
+        double Nx = Math.Pow(i,3);
+        i += 1;
+        Console.WriteLine(Nx);
+    }    
+}
+else 
+{
+    Console.WriteLine("Error");    
+}

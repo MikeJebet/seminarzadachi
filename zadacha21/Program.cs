bcfe0f3b34bf786821157aa4bdb8c0ced211218d
Mikhail <mishaforever96@mail.ru>
2022-08-26 19:32:25 +0300

    zadacha 21

---
+++ zadacha21/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введи координаты точки X через пробел");
+int[] X = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.WriteLine("Введи координаты точки Y через пробел");
+int[] Y = Console.ReadLine().Split().Select(int.Parse).ToArray();
+double dist = Math.Sqrt(Math.Pow(Y[0] - X[0], 2) + Math.Pow(Y[1] - X[1], 2) + Math.Pow(Y[2] - X[2], 2));
+Console.WriteLine(Math.Round(dist, 2));

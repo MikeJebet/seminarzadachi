9c869939bcf84c5f162bce2f7ff0fd61c12de23f
Mikhail <mishaforever96@mail.ru>
2022-09-03 16:54:05 +0300

    zadacha 29

---
+++ zadacha29/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+Console.Write("Введите 8 чисел через пробел: ");
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+for (int i = 0; i < numbers.Length; i++)
+ {
+     Console.Write("| " + numbers[i] + " |");
+ }

c1ba92a0bd584f13ec5d5b77f4ee8706c9cf3de4
Mikhail <mishaforever96@mail.ru>
2022-09-09 19:22:14 +0300

    zadacha 38

---
+++ zadacha38/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Количество элементов в массиве: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[num];
+double result = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = Convert.ToDouble(new Random().Next(1,1000)) / 100;
+    Console.Write("| " + numbers[i] + " |");
+}
+
+result = numbers.Max() - numbers.Min();
+Console.WriteLine($"\n Разница между максимальным числом массива и минимальным: {result}");
+

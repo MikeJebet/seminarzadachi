2272674707926f24845bcba4ee2de305b9cbd7a5
Mikhail <mishaforever96@mail.ru>
2022-09-09 18:51:48 +0300

    zadacha 34

---
+++ zadacha34/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Укажите размер массива: ");
+ int Count = Convert.ToInt32(Console.ReadLine());
+ int SumIndex = 0;
+ int[] arr = new int[Count];
+ Random rnd = new Random();
+ for(int i =0;i<arr.Length;i++)
+{
+    arr[i] = rnd.Next(100,999);
+    if (arr[i] % 2 == 0)
+    {
+        Console.WriteLine(arr[i]);
+        SumIndex += 1;
+    } 
+}
+Console.WriteLine($"Количество чётных чисел в массиве: {SumIndex}");
